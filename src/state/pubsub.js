40db950f1e0f0f40bd7135384d3975183d74f5dd
[JohnPaulHarold] <johnpaul.harold@gmail.com>
2023-08-04 14:50:48 +0100

    [crt68] prettier pass on src/state

--- src/state/pubsub.js
+++ src/state/pubsub.js
@@ -4,39 +4,40 @@
 
 /** @type {StoreType} */
 export const store = {
-  listeners: {},
+    listeners: {},
 
-  broadcast(payload) {
-    Object.keys(this.listeners).forEach((key) => {
-      this.triggerListener(key, payload)
-    })
-  },
+    broadcast(payload) {
+        Object.keys(this.listeners).forEach((key) => {
+            this.triggerListener(key, payload);
+        });
+    },
 
-  triggerListener(id, payload) {
-    const callbacks = this.listeners[id];
+    triggerListener(id, payload) {
+        const callbacks = this.listeners[id];
 
-    if (callbacks && callbacks.length) {
-      callbacks.forEach(callback => callback(payload));
-    }
-  },
+        if (callbacks && callbacks.length) {
+            callbacks.forEach((callback) => callback(payload));
+        }
+    },
 
-  listen(id, callback) {
-    if (!callback || typeof callback !== 'function') {
-      console.warn(`You must pass a function as the second argument to store.listen()`);
-    }
+    listen(id, callback) {
+        if (!callback || typeof callback !== 'function') {
+            console.warn(
+                `You must pass a function as the second argument to store.listen()`
+            );
+        }
 
-    this.listeners[id] = this.listeners[id] || [];
-    this.listeners[id].push(callback);
-  },
+        this.listeners[id] = this.listeners[id] || [];
+        this.listeners[id].push(callback);
+    },
 
-  /**
-   * 
-   * @param {string} id 
-   */
-  unlisten(id) {
-    if (this.listeners[id]) {
-      delete this.listeners[id];
-    }
-
-  }
-}
+    /**
+     *
+     * @param {string} id
+     */
+    unlisten(id) {
+        if (this.listeners[id]) {
+            delete this.listeners[id];
+        }
+    },
+};
