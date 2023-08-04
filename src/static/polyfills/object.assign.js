d3be1d9823097bf28e4ed0a8ec00028e6b69cd36
[JohnPaulHarold] <johnpaul.harold@gmail.com>
2023-08-04 14:55:06 +0100

    [crt69] prettier pass on src/static

--- src/static/polyfills/object.assign.js
+++ src/static/polyfills/object.assign.js
@@ -1,22 +1,22 @@
 // @ts-nocheck
 if (typeof Object.assign != 'function') {
-  Object.assign = function (target) {
-    'use strict';
-    if (target == null) {
-      throw new TypeError('Cannot convert undefined or null to object');
-    }
-
-    target = Object(target);
-    for (var index = 1; index < arguments.length; index++) {
-      var source = arguments[index];
-      if (source != null) {
-        for (var key in source) {
-          if (Object.prototype.hasOwnProperty.call(source, key)) {
-            target[key] = source[key];
-          }
+    Object.assign = function (target) {
+        'use strict';
+        if (target == null) {
+            throw new TypeError('Cannot convert undefined or null to object');
         }
-      }
-    }
-    return target;
-  };
+
+        target = Object(target);
+        for (var index = 1; index < arguments.length; index++) {
+            var source = arguments[index];
+            if (source != null) {
+                for (var key in source) {
+                    if (Object.prototype.hasOwnProperty.call(source, key)) {
+                        target[key] = source[key];
+                    }
+                }
+            }
+        }
+        return target;
+    };
 }
