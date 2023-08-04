0d314489857921c038ecf7495f7f1c5aa0cf916a
[JohnPaulHarold] <johnpaul.harold@gmail.com>
2023-08-04 16:59:15 +0100

    [crt87] prettier pass on src/declarations

--- src/declarations/lrud-spatial.d.ts
+++ src/declarations/lrud-spatial.d.ts
@@ -1,10 +1,8 @@
 declare module '@bbc/tv-lrud-spatial' {
-  function getNextFocus(
-    currentFocus: HTMLElement,
-    keyCode: number,
-    scope?: HTMLElement
-  ): HTMLElement | null
-  function getNextFocus(
-  ): HTMLElement | null
+    function getNextFocus(
+        currentFocus: HTMLElement,
+        keyCode: number,
+        scope?: HTMLElement
+    ): HTMLElement | null;
+    function getNextFocus(): HTMLElement | null;
 }
-
--- src/declarations/types.d.ts
+++ src/declarations/types.d.ts
@@ -2,143 +2,147 @@ import { Orientation } from '../enums/Orientation';
 import { BaseView } from '../libs/baseView';
 
 export interface Outlet {
-  id: string;
+    id: string;
 }
 
 export interface AppOutlets {
-  [index: string]: Outlet;
+    [index: string]: Outlet;
 }
 
-export type RouteParams = Record<string, string | number | boolean>
-export type RouteSearch = Record<string, string | number | boolean>
+export type RouteParams = Record<string, string | number | boolean>;
+export type RouteSearch = Record<string, string | number | boolean>;
 
 export interface Route {
-  default?: boolean;
-  pattern: string;
-  title?: string;
-  id: string;
-  viewClass: typeof BaseView;
-  handler: ({ route: Route, params: RouteParams, search: RouteSearch }) => void;
+    default?: boolean;
+    pattern: string;
+    title?: string;
+    id: string;
+    viewClass: typeof BaseView;
+    handler: ({
+        route: Route,
+        params: RouteParams,
+        search: RouteSearch,
+    }) => void;
 }
 
 export interface ViewOptions {
-  title?: string;
-  id: string;
-  params: RouteParams
-  search: RouteSearch
+    title?: string;
+    id: string;
+    params: RouteParams;
+    search: RouteSearch;
 }
 
 export interface NavViewOptions extends ViewOptions {
-  navItems: NavItem[];
+    navItems: NavItem[];
 }
 
-export type StylesRecord = Record<string, string>
+export type StylesRecord = Record<string, string>;
 
 export interface RailItem {
-  title: string;
-  id: string;
-  url: string;
+    title: string;
+    id: string;
+    url: string;
 }
 export interface RailData {
-  title?: string;
-  id: string;
-  orientation?: Orientation
-  items: RailItem[];
+    title?: string;
+    id: string;
+    orientation?: Orientation;
+    items: RailItem[];
 }
 export interface PageData {
-  title?: string;
-  id: string;
-  items: RailData[];
+    title?: string;
+    id: string;
+    items: RailData[];
 }
 export interface TileDataItem {
-  url: string
+    url: string;
 }
 
 interface BaseComponentProps {
-  id?: string;
-  className?: sring;
+    id?: string;
+    className?: sring;
 }
 
 export interface SimpleCarouselProps extends BaseComponentProps {
-  id: string;
-  data: RailData
-  blockExit?: string;
+    id: string;
+    data: RailData;
+    blockExit?: string;
 }
 
 export interface CarouselProps extends BaseComponentProps {
-  id: string;
-  title?: string;
-  orientation: Orientation;
-  childQuery?: string;
-  blockExit?: string;
+    id: string;
+    title?: string;
+    orientation: Orientation;
+    childQuery?: string;
+    blockExit?: string;
 }
 
 export interface NavProps {
-  navItems: NavItem[];
-  id: string;
-  blockExit?: string;
+    navItems: NavItem[];
+    id: string;
+    blockExit?: string;
 }
 
 export interface TileProps {
-  id: string;
-  title: string;
+    id: string;
+    title: string;
 }
 
 export interface KeyboardKey {
-  display: string;
-  value: string;
-  width?: number;
+    display: string;
+    value: string;
+    width?: number;
 }
 
 export interface KeyProps extends KeyboardKey {
-  className?: string;
+    className?: string;
 }
 
 export interface KeyboardProps {
-  keyMap: Array<Array<KeyboardKey>>
+    keyMap: Array<Array<KeyboardKey>>;
 }
 
 export interface NavItem {
-  id: string,
-  title?: string,
-  href: string
+    id: string;
+    title?: string;
+    href: string;
 }
 
-type ListenerCallback = (payload: any) => void
+type ListenerCallback = (payload: any) => void;
 
 interface ListenerPayload {
-  type: string;
-  nextIndex?: number;
-  offset?: number;
-  direction?: Direction;
+    type: string;
+    nextIndex?: number;
+    offset?: number;
+    direction?: Direction;
 }
 export interface StoreType {
-  listeners: Record<string, ListenerCallback[]>;
-  broadcast: (payload: any) => void;
-  triggerListener: (id: string, payload: ListenerPayload) => void;
-  listen: (id: string, callback: any) => void;
-  unlisten: (id: string) => void;
+    listeners: Record<string, ListenerCallback[]>;
+    broadcast: (payload: any) => void;
+    triggerListener: (id: string, payload: ListenerPayload) => void;
+    listen: (id: string, callback: any) => void;
+    unlisten: (id: string) => void;
 }
 
 export interface GridProps extends BaseComponentProps {
-  data: any;
-  columns: number;
+    data: any;
+    columns: number;
 }
 
 export interface SpinnerProps extends BaseComponentProps {
-  message?: string;
+    message?: string;
 }
 
 export interface LazyImageProps extends BaseComponentProps {
-  src: string;
+    src: string;
 }
 
 export interface SpinnerProps extends BaseComponentProps {
-  message?: string;
+    message?: string;
 }
 
 // note:
 // props: Record<string, any>
 // textOrArray: any
 // children: ?
-export type ShorthandMakeElement = (...args: any[]) => HTMLElement
+export type ShorthandMakeElement = (...args: any[]) => HTMLElement;
