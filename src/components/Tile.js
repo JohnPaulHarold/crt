60fb018e816717568086ec95b9dd42f54cc0da51
[JohnPaulHarold] <johnpaul.harold@gmail.com>
2023-08-16 06:59:41 +0100

    [crt96] fix bad merge

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -6,15 +6,9 @@ import { div, span } from '../libs/makeElement';
 
 import s from './Tile.css';
 /**
-<<<<<<< HEAD
  *
  * @param {TileProps} props
  * @returns {Element}
-=======
- * 
- * @param {TileProps} props 
- * @returns {HTMLElement}
->>>>>>> 929b6e1 ([crt/11] show page)
  */
 export const Tile = (props) => {
     return div(
