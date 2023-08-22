ba15addda4a1366218981e8acbc267f3a15e4709
Edoardo Gargano <cnt000@gmail.com>
2023-08-22 16:20:38 +0200

    fix: MSE check break the page

--- src/utils/isCodecSupported.js
+++ src/utils/isCodecSupported.js
@@ -4,8 +4,8 @@ const video = document.createElement('video');
  * @param {string} codec
  */
 export function isCodecSupported(codec) {
-  return {
-    mse: window.MediaSource?.isTypeSupported(codec),
-    video: !!video.canPlayType(codec),
-  };
+    return {
+        mse: window?.MediaSource?.isTypeSupported?.(codec),
+        video: !!video.canPlayType(codec),
+    };
 }
