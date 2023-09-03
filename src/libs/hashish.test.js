e4d678ee4072b8f302d4d1e178e973f9ef946315
John-Paul Harold <johnpaul.harold@gmail.com>
2023-09-03 15:55:42 +0100

    [crt152] component tests (#155)

--- src/libs/hashish.test.js
+++ src/libs/hashish.test.js
@@ -2,49 +2,49 @@
  * @vitest-environment jsdom
  */
 
-import { describe, expect, test, vi } from "vitest";
-import { Hashish } from "./hashish";
+import { describe, expect, test, vi } from 'vitest';
+import { Hashish } from './hashish';
 
 describe('hashish', () => {
-  test('it registers handlers', () => {
-    const vc = vi.fn();
-    const cb = vi.fn();
-    const id = 'xyz'
-    const r = {
-      pattern: `/${id}`,
-      id,
-      viewClass: vc,
-    }
-    const evt = new HashChangeEvent('hashchange', {
-      newURL: window.location.origin + `#/${id}`
-    })
+    test('it registers handlers', () => {
+        const vc = vi.fn();
+        const cb = vi.fn();
+        const id = 'xyz';
+        const r = {
+            pattern: `/${id}`,
+            id,
+            viewClass: vc,
+        };
+        const evt = new HashChangeEvent('hashchange', {
+            newURL: window.location.origin + `#/${id}`,
+        });
 
-    Hashish.config(window.location.pathname);
-    Hashish.registerRoute(r, cb);
+        Hashish.config(window.location.pathname);
+        Hashish.registerRoute(r, cb);
 
-    window.dispatchEvent(evt)
-    const expectation = {params: {}, search: {}, pattern: `/${id}` }
-    expect(cb).toHaveBeenCalledWith(expectation);
-  });
+        window.dispatchEvent(evt);
+        const expectation = { params: {}, search: {}, pattern: `/${id}` };
+        expect(cb).toHaveBeenCalledWith(expectation);
+    });
 
-  test('it handles a custom "hash"', () => {
-    const vc = vi.fn();
-    const cb = vi.fn();
-    const id = 'xyz'
-    const r = {
-      pattern: `/${id}`,
-      id,
-      viewClass: vc,
-    }
-    const evt = new HashChangeEvent('hashchange', {
-      newURL: window.location.origin + `!#/${id}`
-    })
+    test('it handles a custom "hash"', () => {
+        const vc = vi.fn();
+        const cb = vi.fn();
+        const id = 'xyz';
+        const r = {
+            pattern: `/${id}`,
+            id,
+            viewClass: vc,
+        };
+        const evt = new HashChangeEvent('hashchange', {
+            newURL: window.location.origin + `!#/${id}`,
+        });
 
-    Hashish.config(window.location.pathname, "!#");
-    Hashish.registerRoute(r, cb);
+        Hashish.config(window.location.pathname, '!#');
+        Hashish.registerRoute(r, cb);
 
-    window.dispatchEvent(evt)
-    const expectation = {params: {}, search: {}, pattern: `/${id}` }
-    expect(cb).toHaveBeenCalledWith(expectation);
-  });
+        window.dispatchEvent(evt);
+        const expectation = { params: {}, search: {}, pattern: `/${id}` };
+        expect(cb).toHaveBeenCalledWith(expectation);
+    });
 });
