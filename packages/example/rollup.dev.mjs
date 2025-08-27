774412c7e4a1954c8c74746307fa3de9dfe2f0de
[JohnPaulHarold] <johnpaul.harold@gmail.com>
2025-08-27 10:00:00 +0100

    CRT013: add .env support

--- packages/example/rollup.dev.mjs
+++ packages/example/rollup.dev.mjs
@@ -1,10 +1,20 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import serve from 'rollup-plugin-serve';
 import config from './rollup.config.mjs';
+
+// Resolve the path to the monorepo root's .env file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+const PORT = process.env.PORT || 3000; // Use PORT from .env or default to 3000
 
 config.plugins.push(
 	serve({
 		open: true,
-		port: 20001,
+		port: PORT,
 		contentBase: 'dist',
 	})
 );
